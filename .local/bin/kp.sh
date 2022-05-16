db549758ad433bd7596732e71d68e155a4709f30
hattori-hanz0 <hattori@tuta.io>
2022-05-16 17:33:11 +0200

    2022-05-16-17:33:11

--- .local/bin/kp.sh
+++ .local/bin/kp.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-PROCES=$(ps ax | fzf -m --height=100% | awk '{print $1}')
+PROCES=$(ps -hxo pid,cmd | fzf -m --height=100% --header='[kill:process]' --prompt "Wyszukaj: " | awk '{print $1}')
 [ -z $PROCES ] || kill -9 $PROCES
