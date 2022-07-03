521280857c8c74a89be9e5915f3a89dc79437871
hattori-hanz0 <hattori@tuta.io>
2022-07-03 16:50:37 +0200

    2022-07-03-16:50:37

--- .local/bin/kp.sh
+++ .local/bin/kp.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
-PROCES=$(ps -hxo pid,cmd | fzf -m --height=100% --header='[kill:process]' --prompt "Wyszukaj: " | awk '{print $1}')
-[ -z $PROCES ] || kill -9 $PROCES
+PROCESY=$(ps -hxo pid,cmd | fzf -m --reverse --height=100% --header='[kill:process]' --prompt "Wyszukaj: " | awk '{print $1}')
+
+PROCESY_ARRAY=($PROCESY)
+
+for ITEM in "${PROCESY_ARRAY[@]}"; do
+    echo "Zabijam proces $ITEM :("
+    kill -9 $ITEM
+done
